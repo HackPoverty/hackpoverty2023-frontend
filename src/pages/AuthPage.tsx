50c5fae9a425b27febda4bc17f6d58b8678471cc
Viet Hung Nguyen <vhn@sfu.ca>
2023-02-18 20:17:03 -0800

    Navigation guards with temporary login and logout

--- src/pages/AuthPage.tsx
+++ src/pages/AuthPage.tsx
@@ -1,10 +1,22 @@
 import { IonButton, IonContent, IonPage } from "@ionic/react";
+import { useHistory } from "react-router";
+import { useAuth } from "src/auth/store";
 
 export const AuthPage = () => {
+  const auth = useAuth();
+  const history = useHistory();
   return (
     <IonPage>
       <IonContent>
-        <IonButton slot="fixed">Log in to continue</IonButton>;
+        <IonButton onClick={() => {
+          auth.login("FARMER");
+          history.push("/dashboard");
+        }}>Log in as farmer</IonButton>
+
+        <IonButton onClick={() => {
+          auth.login("TECHNICIAN");
+          history.push("/farmers");
+        }}>Log in as technician</IonButton>
       </IonContent>
     </IonPage>
   );
