9dc607e2eab2c36ee26be5b99b26f2172d6747f8
Viet Hung Nguyen <vhn@sfu.ca>
2023-02-22 01:34:24 -0800

    Add query client to the test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,8 +1,14 @@
-import React from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { render } from "@testing-library/react";
 import App from "./App";
 
+
 test("renders without crashing", () => {
-  const { baseElement } = render(<App />);
+  const queryClient = new QueryClient();
+  const { baseElement } = render(
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
+  );
   expect(baseElement).toBeDefined();
 });
