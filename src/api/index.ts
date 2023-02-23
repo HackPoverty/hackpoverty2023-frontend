7732a67bf46672354b042a3248fe3e2b8e640760
Viet Hung Nguyen <vhn@sfu.ca>
2023-02-22 23:34:52 -0800

    Construct the API

---
+++ src/api/index.ts
@@ -0,0 +1,32 @@
+import axios from "axios"
+import { CaseType, deserialize } from "jsonapi-fractal"
+import { TOKEN_STORAGE_KEY } from "src/auth"
+
+const BASE_URL = "http://chicken.albernihosting.ca" as const
+
+const baseApi = axios.create({
+  baseURL: BASE_URL,
+})
+
+const jsonApi = axios.create({
+  baseURL: `${BASE_URL}/jsonapi_data`,
+})
+
+// Attach the storage key for every jsonApi request
+jsonApi.interceptors.request.use(config => {
+  config.headers.Authorization = `Bearer ${localStorage.getItem(TOKEN_STORAGE_KEY) || ""}`
+  return config
+})
+
+// This one smells
+function jsonDeserialize<Type>(data: never) {
+  return deserialize<Type>(data, { changeCase: CaseType.camelCase }) as Type
+}
+
+export {
+  baseApi,
+  jsonApi,
+  jsonDeserialize,
+}
+
+
