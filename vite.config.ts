afdc6b856c79201290645280302aa79ce73e967b
Viet-Hung Nguyen <41867185+viethung7899@users.noreply.github.com>
2023-02-24 00:33:14 -0800

    Set up PWA (#15)
    * Prepare setting for PWA
    
    * Remove CRA service worker

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,30 @@
 /// <reference types="vitest" />
-import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
+import { VitePWA } from "vite-plugin-pwa"
 import viteTsconfigPaths from "vite-tsconfig-paths"
-import svgrPlugin from "vite-plugin-svgr"
+import manifest from "./manifest.json"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), viteTsconfigPaths(), svgrPlugin()],
+  plugins: [
+    react(),
+    viteTsconfigPaths(),
+    VitePWA({
+      manifest,
+      includeAssets: [
+        "aseets/icon/favicon.ico",
+        "aseets/icon/apple-touch-icon.png",
+        "aseets/icon/favicon.png",
+      ],
+      devOptions: {
+        enabled: true,
+      },
+      workbox: {
+        globPatterns: ["**/*.{js,css,html}", "**/*.{svg,png,jpg,gif}"],
+      },
+    }),
+  ],
   build: {
     outDir: "build",
   },
