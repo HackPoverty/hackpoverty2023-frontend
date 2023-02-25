e9651dcf6215652aee1b561b0cab10baf41c2b54
Angelo Aldecoa <angelokn14@gmail.com>
2023-02-25 12:48:03 -0800

    ensure farmer journal type matches the one from backend and create farmer journal post function

--- src/api/farmer.ts
+++ src/api/farmer.ts
@@ -1,9 +1,24 @@
-import { FarmerJournal } from "src/types/contentTypes"
+import { serialize, CaseType } from "jsonapi-fractal"
 import { jsonApi, jsonDeserialize } from "."
+import { FarmerJournal } from "src/types/contentTypes"
 
 export const getFarmerJournalLogs = async (uid: string) => {
   const reponse = await jsonApi.get(
     `node/farmer_daily_journal?filter[uid.meta.drupal_internal__target_id]=${uid}`
   )
   return jsonDeserialize<FarmerJournal[]>(reponse.data)
+}
+
+export const postFarmerJournal = async (journal: FarmerJournal) => {
+  return jsonApi.post(
+    `node/farmer_daily_journal`,
+    serialize<FarmerJournal>(journal, "node--farmer_daily_journal", {
+      changeCase: CaseType.snakeCase,
+    }),
+    {
+      headers: {
+        "Content-Type": "application/vnd.api+json",
+      },
+    }
+  )
 }
--- src/types/contentTypes.ts
+++ src/types/contentTypes.ts
@@ -1,72 +1,70 @@
-import { Node } from './highLevelTypes';
+import { Node } from "./highLevelTypes"
 
 /**
  * Geoinformation for a Drupal Geofield
  */
 export type GPSCoordinates = {
-  value: string;
-  geoType: string;
+  value: string
+  geoType: string
 
   /** The Latitude */
-  lat: number;
+  lat: number
 
   /** The Longitude */
-  lon: number;
+  lon: number
 
-  left: number;
-  top: number;
-  right: number;
-  bottom: number;
-  geohash: string;
-  latlon: string;
+  left: number
+  top: number
+  right: number
+  bottom: number
+  geohash: string
+  latlon: string
 }
 
 /** The possible values for the field_diease field */
-export type PresenceOfDisease = ['Yes', 'No', 'Possible'];
+export type PresenceOfDisease = ["Yes", "No", "Possible"]
 
 /** The possible values for the scale fields */
-export type DiseaseScale = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+export type DiseaseScale = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 /**
  * The `node--technician_visit` type
  */
 export type TechnicianVisit = Node & {
-  fieldVisitComments: null,
+  fieldVisitComments: null
 
   /** GPS coordinates of where the visit was taken */
-  fieldGpsCoordinates: GPSCoordinates|null,
+  fieldGpsCoordinates: GPSCoordinates | null
 
   /** The likelihood of the presence of a disease */
-  fieldDisease: PresenceOfDisease[number],
+  fieldDisease: PresenceOfDisease[number]
 
   /** Common disease names */
-  fieldDiseaseNames: string|null,
+  fieldDiseaseNames: string | null
 
   /** An other field in case disease is not among common options */
-  fieldOtherpossibledisease: string|null,
-
+  fieldOtherpossibledisease: string | null
 
   /** Whether a vaccine was given or not */
-  fieldVaccineGiven: boolean,
+  fieldVaccineGiven: boolean
 
   /** Common vaccines that were administered */
-  fieldVaccinations: string[],
+  fieldVaccinations: string[]
 
   /** List the name of a vaccine that is not among the previous options */
-  fieldOtherVaccine: string|null,
-
+  fieldOtherVaccine: string | null
 
   /** Checklist field: how clean the bedding is */
-  fieldCleanBedding: DiseaseScale[number],
+  fieldCleanBedding: DiseaseScale[number]
 
   /** Checklist field: how good the feed is */
-  fieldFeedQuantity: DiseaseScale[number],
+  fieldFeedQuantity: DiseaseScale[number]
 
   /** Checklist field: whether chickens are getting enough light */
-  fieldLightSufficiency: DiseaseScale[number],
+  fieldLightSufficiency: DiseaseScale[number]
 
   /** Checklist field: whether the chicken pens are well ventilated */
-  fieldVentillation: DiseaseScale[number],
+  fieldVentillation: DiseaseScale[number]
 
   /** Checklist field: how clean the water is */
   fieldWaterCleanliness: DiseaseScale[number]
@@ -77,42 +75,45 @@ export type TechnicianVisit = Node & {
  */
 export type FarmerJournal = Node & {
   /** The number of chickens that are alive */
-  fieldClosingStock: number|null,
+  fieldClosingStock: number | null
 
   /** The number of damaged eggs */
-  fieldDamagedEggs: number|null,
+  fieldDamagedEggs: number | null
 
   /** The percentage of eggs that are damaged */
-  fieldDamagedEggsPercentage_: number|null,
+  fieldDamagedEggsPercentage_: number | null
 
   /** The total number of eggs produced */
-  fieldEggsProduced: number|null,
+  fieldProducedEggs: number | null
 
   /** The number of large eggs */
-  fieldLargeEggs: number|null,
+  fieldLargeEggs: number | null
 
   /** The number of medium eggs */
-  fieldMediumEggs: number|null,
+  fieldMediumEggs: number | null
 
   /** The number of small eggs */
-  fieldSmallEggs: number|null,
+  fieldSmallEggs: number | null
 
   /** The amoung of feed given to the birds (in "bags")
    * FIXME: this should be in kgs later on
    */
-  fieldGivenFeed: number|null,
-  fieldGramsPerBird: number|null,
+  fieldGivenFeed: number | null
+  fieldGramsPerBird: number | null
 
   /** The industry standard of grams per bird. A relatively static variable */
-  fieldGramPerBirdIndustrySta: number|null,
+  fieldGramPerBirdIndustrySta: number | null
 
-  fieldHoursOfLight: number|null,
-  fieldInitialStock: number|null,
-  fieldLayFrequency: number|null,
-  fieldLayFrequencyIndustrySta: number|null,
-  fieldMortality: number|null,
-  fieldMortalityPercentage_: number|null,
-  fieldMortalityProlapse_: number|null,
-  fieldTotalMortality: number|null,
-  fieldWeightOfBird: number|null
+  fieldHoursOfLight: number | null
+  fieldInitialStock: number | null
+  fieldLayFrequency: number | null
+  fieldLayFrequencyIndustrySta: number | null
+  fieldMortality: number | null
+  fieldMortalityPercentage_: number | null
+  fieldMortalityProlapse_: number | null
+  fieldTotalMortality: number | null
+  fieldWeightOfBird: number | null
+
+  //* notes for organization */
+  fieldCommentdailycheck: string | null
 }
