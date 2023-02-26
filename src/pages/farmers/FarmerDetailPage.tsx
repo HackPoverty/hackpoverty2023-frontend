00d5e07fb1754ab66713cf2e13e5e44d0f3b485f
Viet Hung Nguyen <vhn@sfu.ca>
2023-02-25 18:43:20 -0800

    Add statistics

--- src/pages/farmers/FarmerDetailPage.tsx
+++ src/pages/farmers/FarmerDetailPage.tsx
@@ -7,17 +7,36 @@ import {
   IonHeader,
   IonPage,
   IonTitle,
-  IonToolbar,
+  IonToolbar
 } from "@ionic/react"
-import { useQueries } from "@tanstack/react-query"
+import { useQueries, useQuery } from "@tanstack/react-query"
 import dayjs from "dayjs"
 import relativeTime from "dayjs/plugin/relativeTime"
 import { t } from "i18next"
 import { useParams } from "react-router"
+import { getFarmerJounalLogsById } from "src/api/farmer"
 import { getTechnicianVisits } from "src/api/technician"
 import { getFarmerById } from "src/api/users"
 
 dayjs.extend(relativeTime)
+
+const useFarmerSatistics = (farmerId: string) => {
+  const { data, isLoading, error } = useQuery(["farmer_journal", farmerId], () => getFarmerJounalLogsById(farmerId))
+
+  const lastWeek = data?.filter(item => dayjs(item.created) > dayjs().subtract(7, "days"))
+  const totalMortiality = lastWeek?.reduce((prev, curr) => prev + (curr.fieldMortality || 0), 0)
+  const totalEggProduction = lastWeek?.reduce((prev, curr) => prev + (curr.fieldProducedEggs || 0), 0)
+  const totalFeedGram = lastWeek?.reduce((prev, curr) => prev + (curr.fieldGramPerBirdIndustrySta || 0), 0)
+
+  return {
+    isLoading,
+    error,
+    totalMortiality,
+    avgEggProduciton: lastWeek && lastWeek.length > 0 && totalEggProduction !== undefined ? totalEggProduction / lastWeek.length : undefined,
+    avgFeed: lastWeek && lastWeek.length > 0 && totalFeedGram !== undefined ? totalFeedGram / lastWeek.length : undefined,
+  }
+}
+
 
 export const FarmerDetailPage = () => {
   const { farmer_id } = useParams<{ farmer_id: string }>()
@@ -27,6 +46,8 @@ export const FarmerDetailPage = () => {
       { queryKey: ["technicalVisits", farmer_id], queryFn: () => getTechnicianVisits(farmer_id) },
     ],
   })
+  const { isLoading, totalMortiality, avgEggProduciton, avgFeed } = useFarmerSatistics(farmer_id)
+
   return (
     <IonPage>
       <IonHeader>
@@ -49,13 +70,18 @@ export const FarmerDetailPage = () => {
         >
           Record Entry
         </IonButton>
+        <h4>Last Week Statistics</h4>
+        {isLoading ? <p>Loading statistics...</p> : <>
+          <p>Total mortiality: {totalMortiality}</p>
+          <p>Average egg production: {avgEggProduciton || "N/A"}</p>
+          <p>Average feed per bird production: {avgFeed || "N/A"}</p>
+        </>}
         <h4>Past Visit Records</h4>
         {pastVisits.isLoading ? <p>Retrieving farmer profile...</p> :
           !pastVisits.data ? <p>Cannot retrive farmer profile</p> : null}
         {pastVisits.data?.map((visit) => (
           <PastVisitRecord technicianVisit={visit} key={visit.id} />
         ))}
-        <h4>Farmer Records</h4>
       </IonContent>
     </IonPage>
   )
