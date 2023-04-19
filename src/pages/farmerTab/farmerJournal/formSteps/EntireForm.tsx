4bf8ef5f25995ae944f2c0559c019bcd6cbf7cdd
Nathan Tsai <nathan@albernidesign.com>
2023-04-19 07:29:28 -0700

    Try getting the "Entire Form" to work.

---
+++ src/pages/farmerTab/farmerJournal/formSteps/EntireForm.tsx
@@ -0,0 +1,206 @@
+import {
+  IonContent,
+  IonButton,
+  IonInput,
+  IonItem,
+  IonLabel,
+  IonList,
+  IonListHeader,
+  IonNavLink,
+} from "@ionic/react"
+import { useFormContext, useWatch } from "react-hook-form"
+import { useTranslation } from "react-i18next"
+import { FarmerJournalFormInputs } from ".."
+import { FormStepE } from "./FormStepE"
+import React, { useMemo } from "react"
+
+export const EntireForm = () => {
+  const { t } = useTranslation();
+  const { register, watch, setValue, getValues } = useFormContext<FarmerJournalFormInputs>()
+    
+
+  const useFormValues = () => {
+    const { getValues } = useFormContext();
+  
+    return {
+      ...useWatch(), // subscribe to form value updates
+      ...getValues(), // always merge with latest form values
+    }
+  }
+
+  const values = useFormValues();
+  
+  // Callback version of watch.  It's your responsibility to unsubscribe when done.
+  React.useEffect(() => {      
+    const subscription = watch((value, { name }) => {
+      
+      switch(name) {
+        case "closingStock":
+        case "totalFeedAmount":
+          if (value.totalFeedAmount != null && value.closingStock != null) {
+            setValue("feedGramsPerBird", value.totalFeedAmount / value.closingStock);
+          }
+          break;
+
+        case "mortality":
+        case "mortalityProlapse":
+          if (value.mortality != null && value.mortalityProlapse != null) {
+            setValue("mortalityTotal", value.mortality + value.mortalityProlapse);
+          }
+          break;
+
+        case "initialStock":
+        case "mortalityTotal":
+          if (value.initialStock != null && value.mortalityTotal != null) {
+            setValue("closingStock", value.initialStock - value.mortalityTotal);
+            setValue("mortalityPercent", (100.0 * value.mortalityTotal/value.initialStock) );
+          }
+          break;
+
+        case "smallEggsCount":
+        case "mediumEggsCount":
+        case "largeEggsCount":
+          if (value.smallEggsCount != null && value.mediumEggsCount != null && value.largeEggsCount != null) {
+            setValue("totalEggsProduced", value.smallEggsCount + value.mediumEggsCount + value.largeEggsCount);
+          }
+          break;
+      }
+    });
+    return () => subscription.unsubscribe();
+  }, [watch, setValue, values]);
+
+
+  // const updateFeedPerBird = () => {
+  //   const value = getValues();
+  //   if (value.totalFeedAmount && value.closingStock) {
+  //     setValue("feedGramsPerBird", value.totalFeedAmount / value.closingStock);
+  //   }
+  // };
+
+  // const [mortality, mortality_prolapse] = watch(["mortality", "mortalityProlapse"]);
+
+  // if (mortality && mortality_prolapse) {
+  //   setValue("mortalityTotal", mortality + mortality_prolapse);
+  // }
+
+  // useMemo(() => {
+  //   console.log("hello from use Memo", values)
+  //   if (mortality !== null && mortality_prolapse !== null) {
+  //     setValue("mortalityTotal", mortality + mortality_prolapse);
+  //   }
+  // }, [mortality, mortality_prolapse]);
+
+
+  // const updateClosingStockAndMortialityPrecent = () => {
+  //   const value = e.target.form.values;
+  //   if (value.initialStock && value.mortalityTotal) {
+  //     setValue("closingStock", value.initialStock - value.mortalityTotal);
+  //     setValue("mortalityPercent", (100.0 * value.mortalityTotal/value.initialStock) );
+  //   }
+  // }
+
+  // const updateEggCount = () => {
+  //   const value = values;
+  //   if (value.smallEggsCount !== null && value.mediumEggsCount !== null && value.largeEggsCount !== null) {
+  //     setValue("totalEggsProduced", value.smallEggsCount + value.mediumEggsCount + value.largeEggsCount);
+  //   }
+  // }
+
+  return (
+    <IonContent className="page">
+      <IonList>
+        <IonListHeader>{t('chickens')}</IonListHeader>
+        <IonItem>
+          <IonLabel position="stacked">{t('initial_stock')}</IonLabel>
+          <IonInput type="number" {...register("initialStock")} />
+        </IonItem>
+        <IonItem>
+          <IonLabel position="stacked">{t('mortality')} ({t('number_of_chicken_deaths')})</IonLabel>
+          <IonInput type="number" {...register("mortality")} />
+        </IonItem>
+        <IonItem>
+          <IonLabel position="stacked">{t('mortality_prolapse')}</IonLabel>
+          <IonInput type="number" {...register("mortalityProlapse")} />
+        </IonItem>
+        <IonItem>
+          <IonLabel position="stacked">{`Total Mortality`}</IonLabel>
+          <IonInput type="number" {...register("mortalityTotal")} disabled />
+        </IonItem>
+        <IonItem>
+          <IonLabel position="stacked">{`Total Mortality Percent`}</IonLabel>
+          <IonInput type="number" step="0.01" {...register("mortalityPercent")} disabled />
+        </IonItem>
+        <IonItem>
+          <IonLabel position="stacked">{"Closing Stock"}</IonLabel>
+          <IonInput type="number" {...register("closingStock")} disabled />
+        </IonItem>
+        <IonItem>
+          <IonLabel position="stacked">{t('large_eggs_produced')}</IonLabel>
+          <IonInput type="number" {...register("largeEggsCount")} />
+        </IonItem>
+        <IonItem>
+          <IonLabel position="stacked">{t('medium_eggs_produced')}</IonLabel>
+          <IonInput type="number" {...register("mediumEggsCount")} />
+        </IonItem>
+        <IonItem>
+          <IonLabel position="stacked">{t('small_eggs_produced')}</IonLabel>
+          <IonInput type="number" {...register("smallEggsCount")} />
+        </IonItem>
+        <IonItem>
+          <IonLabel position="stacked">{`Total Eggs Produced`}</IonLabel>
+          <IonInput type="number" value={getValues("totalEggsProduced")} disabled />
+        </IonItem>
+        <IonItem>
+          <IonLabel position="stacked">{t('number_of_damage_eggs')}</IonLabel>
+          <IonInput type="number" {...register("damagedEggsCount")} />
+        </IonItem>
+        <IonItem>
+          <IonLabel position="stacked">{`Percentage of Damaged Eggs`}</IonLabel>
+          <IonInput type="number" {...register("damagedEggsPercentage")} disabled />
+        </IonItem>
+        <IonItem>
+          <IonLabel position="stacked">{t('lay_frequency')}</IonLabel>
+          <IonInput type="number" {...register("layFrequency")} />
+        </IonItem>
+        <IonItem>
+          <IonLabel position="stacked">{t('industry_standard')}</IonLabel>
+          <IonInput
+            type="number"
+            {...register("layFrequencyIndustryStandard")}
+          />
+        </IonItem>
+        <IonItem>
+          <IonLabel position="stacked">{t('total_feed_given')}</IonLabel>
+          <IonInput type="number" {...register("totalFeedAmount")} />
+        </IonItem>
+        <IonItem>
+          <IonLabel position="stacked">{`Grams per Bird`}</IonLabel>
+          <IonInput type="number" {...register("feedGramsPerBird")} disabled />
+        </IonItem>
+        <IonItem>
+          <IonLabel position="stacked">{t('industry_standard')}</IonLabel>
+          <IonInput
+            type="number"
+            {...register("totalFeedAmountIndustryStandard")}
+          />
+        </IonItem>
+        <IonItem>
+          <IonLabel position="stacked">{`Hours of Light`}</IonLabel>
+          <IonInput
+            type="number"
+            {...register("hoursOfLight")}
+          />
+        </IonItem>
+        <IonItem>
+          <IonLabel position="stacked">
+            {t("notes_and_concerns_for_admins_or_technicians")}
+          </IonLabel>
+          <IonInput type="text" {...register("otherNotes")} />
+        </IonItem>
+      </IonList>
+      <IonNavLink routerDirection="forward" component={() => <FormStepE />}>
+        <IonButton>{t('next')}</IonButton>
+      </IonNavLink>
+    </IonContent>
+  )
+}
--- src/pages/farmerTab/farmerJournal/index.tsx
+++ src/pages/farmerTab/farmerJournal/index.tsx
@@ -10,22 +10,60 @@ import {
   IonTitle,
   IonToolbar,
 } from "@ionic/react"
-import { useForm, SubmitHandler, FormProvider } from "react-hook-form"
+import { useForm, SubmitHandler, FormProvider, useFormContext, useWatch } from "react-hook-form"
 import { FormStepA } from "@/pages/farmerTab/farmerJournal/formSteps/FormStepA"
 import { useRef } from "react"
 import { useTranslation } from "react-i18next"
+import { EntireForm } from "./formSteps/EntireForm"
+import { postFarmerJournal } from "src/api/farmer"
+import { FarmerJournal as FarmJounalType } from "src/types/contentTypes"
+import React from "react"
 
 export const FarmerJournal = () => {
   const { t } = useTranslation();
   const modal = useRef<HTMLIonModalElement>(null)
-  const methods = useForm<FarmerJournalFormInputs>()
+  const methods = useForm<FarmerJournalFormInputs>({
+    defaultValues: {
+      initialStock: 0,
+      mortality: 0,
+      mortalityProlapse: 0,
+      mortalityTotal: 0,
+      mortalityPercent: 0.0,
+      closingStock: 0,
+
+      largeEggsCount: 0,
+      mediumEggsCount: 0,
+      smallEggsCount: 0,
+      totalEggsProduced: 0,
+
+      damagedEggsCount: 0,
+      damagedEggsPercentage: 0.0,
+
+      layFrequency: 0.0,
+      layFrequencyIndustryStandard: 0.0,
+      totalFeedAmount: 0,
+      feedGramsPerBird: 0.0,
+      totalFeedAmountIndustryStandard: 0,
+      hoursOfLight: 0.0,
+      otherNotes: "",
+    },
+  });
+
   const onSubmit: SubmitHandler<FarmerJournalFormInputs> = async (data) => {
+
+    const farmerJournal = convert(data);
     // we pretend the mutation is successful
-    console.log(data)
-    await new Promise((resolve) => setTimeout(resolve, 500))
-    console.log("MUTATION SUCCESS")
+    console.log(farmerJournal)
+    
+    const response = await postFarmerJournal(farmerJournal);
+
+    // await new Promise((resolve) => setTimeout(resolve, 500))
+    console.log(response)
     modal.current?.dismiss()
   }
+
+  const now = new Date();
+  const dateString = now.toLocaleDateString();
 
   return (
     <IonPage>
@@ -39,8 +77,13 @@ export const FarmerJournal = () => {
       </IonHeader>
 
       <IonContent className="ion-padding">
+        
         <IonButton id="open-modal" expand="block">
           {t("record_data")}
+        </IonButton>
+
+        <IonButton id="open-modal-2" expand="block">
+          {t("record_data")} (Entire Form)
         </IonButton>
 
         <IonModal ref={modal} trigger="open-modal">
@@ -51,7 +94,7 @@ export const FarmerJournal = () => {
                   {t("cancel")}
                 </IonButton>
               </IonButtons>
-              <IonTitle>{t("journal_of")} [DATE]</IonTitle>
+              <IonTitle>{t("journal_of")} {dateString}</IonTitle>
             </IonToolbar>
           </IonHeader>
           <IonContent className="ion-padding">
@@ -62,22 +105,84 @@ export const FarmerJournal = () => {
             </FormProvider>
           </IonContent>
         </IonModal>
+        
+        <IonModal ref={modal} trigger="open-modal-2">
+          <IonHeader>
+            <IonToolbar>
+              <IonButtons slot="start">
+                <IonButton onClick={() => modal.current?.dismiss()}>
+                  {t("cancel")}
+                </IonButton>
+              </IonButtons>
+              <IonTitle>{t("journal_of")} {dateString}</IonTitle>
+            </IonToolbar>
+          </IonHeader>
+          <IonContent className="ion-padding">
+            <FormProvider {...methods}>
+              <form onSubmit={methods.handleSubmit(onSubmit)}>
+                <IonNav root={() => <EntireForm />} />
+              </form>
+            </FormProvider>
+          </IonContent>
+        </IonModal>
+
       </IonContent>
     </IonPage>
   )
-}
+};
 
 export type FarmerJournalFormInputs = {
   initialStock: number
   mortality: number
   mortalityProlapse: number
+  mortalityTotal: number
+  mortalityPercent: number
+  closingStock: number
+
   largeEggsCount: number
   mediumEggsCount: number
   smallEggsCount: number
+  totalEggsProduced: number
+
   damagedEggsCount: number
+  damagedEggsPercentage: number
+
   layFrequency: number
   layFrequencyIndustryStandard: number
   totalFeedAmount: number
+  feedGramsPerBird: number
   totalFeedAmountIndustryStandard: number
+  hoursOfLight: number
   otherNotes: string
-}
+};
+
+function convert(inputs: FarmerJournalFormInputs): FarmJounalType {
+  const fj : FarmJounalType = {
+    fieldInitialStock: inputs.initialStock,
+    fieldMortality: inputs.mortality,
+    fieldMortalityProlapse_: inputs.mortalityProlapse,
+    fieldTotalMortality: inputs.mortalityTotal,
+    fieldMortalityPercentage_: inputs.mortalityPercent,
+    fieldClosingStock: inputs.closingStock,
+
+    fieldLayFrequency: inputs.layFrequency,
+    fieldLayFrequencyIndustrySta: inputs.layFrequencyIndustryStandard,
+
+    fieldDamagedEggs: inputs.damagedEggsCount,
+    fieldDamagedEggsPercentage_: inputs.damagedEggsPercentage,
+    fieldEggsProduced: inputs.totalEggsProduced,
+    fieldLargeEggs: inputs.largeEggsCount,
+    fieldMediumEggs: inputs.mediumEggsCount,
+    fieldSmallEggs: inputs.smallEggsCount,
+
+    fieldGivenFeed: inputs.totalFeedAmount,
+    fieldGramsPerBird: inputs.feedGramsPerBird,
+    fieldGramPerBirdIndustrySta: inputs.totalFeedAmountIndustryStandard,
+
+    fieldHoursOfLight: inputs.hoursOfLight,
+
+    fieldWeightOfBird: 0.0,
+  };
+
+  return fj;
+};
